5c706525e05af56253ccc2386ca902a4f82873f1
Moh <mohamad.jamal.mohamad2@gmail.com>
2025-12-09 22:39:00 +0300

    last update

--- database/migrations/2025_12_08_155829_create_bookings_table.php
+++ database/migrations/2025_12_08_155829_create_bookings_table.php
@@ -4,32 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('bookings', function (Blueprint $table) {
-    $table->id();
+            $table->id();
 
-    // Relations
-    $table->foreignId('appartement_id')->constrained()->onDelete('cascade');
-    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            // Relations
+            $table->foreignId('appartement_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
-    // Booking details
-    $table->date('start_date');
-    $table->date('end_date');
-    $table->enum('status', ['available', 'pending
-    ', 'booked', 'cancelled'])->default('available');
+            // Booking details
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['pending
+    ',
+                'booked',
+                'cancelled'
+            ])->default('pending');
 
-    // Optional extras
-    $table->decimal('total_price', 10, 2)->nullable();
-    $table->text('notes')->nullable();
+            // Optional extras
+            $table->decimal('total_price', 10, 2)->nullable();
+            $table->text('notes')->nullable();
 
-    $table->timestamps();
-});
+            $table->timestamps();
+        });
     }
 
     /**
